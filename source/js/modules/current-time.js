0ddaa9123d60e2a4914b3a7668b65896315d2d42
A Zhila <aleksej.zhila@gmail.com>
2022-04-20 23:15:17 +0300

    добавляет адаптацию главной страницы под 320

--- source/js/modules/current-time.js
+++ source/js/modules/current-time.js
@@ -2,17 +2,24 @@ function showCurrentTime() {
   const main = document.querySelector('main');
   const refresh = 1000;
 
+  const container = document.createElement('div');
   const clock = document.createElement('div');
 
   clock.classList.add('text');
   clock.style.cssText = `
     color: white;
-    position: absolute;
-    bottom: 10px;
-    right: 100px;
+    padding: 1rem;
   `;
 
-  main.appendChild(clock);
+  container.style.cssText = `
+    display: flex;
+    align-items: center;
+    justify-content: flex-end;
+    height: 2rem;
+  `;
+
+  main.appendChild(container);
+  container.appendChild(clock);
 
   function updateClock() {
     const time = new Date();
